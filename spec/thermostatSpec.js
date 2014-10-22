c4fe6dcbd514131502f5f9b3dfc84e56ace4dae0
Bernard Mordan <bernardmordan@gmail.com>
2014-10-22 12:00:09 +0100

    Thermostat tests passing

--- spec/thermostatSpec.js
+++ spec/thermostatSpec.js
@@ -1,5 +1,5 @@
 "use strict";
-describe('This amazing thermostat', function() {
+describe('With this amazing thermostat you can', function() {
 
   var thermostat
 
@@ -7,8 +7,84 @@ describe('This amazing thermostat', function() {
     thermostat = new Thermostat;
   })
  
-  it('when you turn it on it is in power saving mode', function() {
+  it('Turn it on. It is in power saving mode', function() {
     expect(thermostat.isPowerSavingOn).toEqual(true)
   })
 
+  it('and has a starting temp of 20.', function() {
+    expect(thermostat.temperature).toEqual(20)
+  })
+
+  describe('Control the Thermostat', function() {
+
+    it('You can increase the temp with the up button', function() {
+      thermostat.increase()
+      expect(thermostat.temperature).toEqual(21)
+    })
+
+    it('You can increase the temp with the up button.', function() {
+      thermostat.decrease()
+      expect(thermostat.temperature).toEqual(19)
+    })
+
+    it('you can increase in bigger steps if you want to', function() {
+      thermostat.increaseTemperatureBy(3)
+      expect(thermostat.temperature).toEqual(23)
+    })
+
+    it('you can decrease in bigger steps if you want to', function() {
+      thermostat.decreaseTemperatureBy(3)
+      expect(thermostat.temperature).toEqual(17)
+    })
+
+    it('It has a min temperature of 10', function() {
+      thermostat.decreaseTemperatureBy(20)
+      expect(thermostat.temperature).toEqual(10)
+    })
+
+    it('In power saving mode you can\'t exceed 25 degrees', function() {
+      thermostat.increaseTemperatureBy(100)
+      expect(thermostat.temperature).toEqual(25)
+    })
+
+    it('but with power saving off you can go upto 35', function() {
+      thermostat.isPowerSavingOn = false
+      thermostat.increaseTemperatureBy(100)
+      expect(thermostat.temperature).toEqual(35)
+    })
+
+    it('you can reset the Thermostat with the reset button', function() {
+      thermostat.increase()
+      thermostat.reset()
+      expect(thermostat.temperature).toEqual(20)
+    })
+
+  });
+
+  describe('Colorise the reading', function() {
+    
+    var rainbow
+
+    beforeEach(function(){
+      rainbow = new Rainbow
+    })
+
+    it('returns color values for the frontend', function(){
+      expect(thermostat.colorise(20)).toEqual('cce600')
+    })
+
+    it('green when below 18', function(){
+      expect(thermostat.colorise(17)).toEqual('8fc700')
+    })
+
+    it('yellow when below 25', function(){
+      expect(thermostat.colorise(24)).toEqual('ffe000')
+    })
+
+    it('red when over 25', function(){
+      expect(thermostat.colorise(29)).toEqual('ff7a00')
+    })
+  });
+
 })
+
--- src/thermostat.js
+++ src/thermostat.js
@@ -1,3 +1,33 @@
 function Thermostat () {
   this.isPowerSavingOn = true
+  this._temperature = 20
 }
+
+Thermostat.prototype = {
+  get temperature() { return this._temperature }
+}
+Thermostat.prototype.increase = function() {
+  this.increaseTemperatureBy(1)
+}
+Thermostat.prototype.increaseTemperatureBy = function(degrees) {
+  this._temperature += degrees
+  if( this.isPowerSavingOn && this.temperature > 25) this._temperature = 25
+  if(!this.isPowerSavingOn && this.temperature > 35) this._temperature = 35
+}
+Thermostat.prototype.decrease = function() {
+  this.decreaseTemperatureBy(1)
+}
+Thermostat.prototype.decreaseTemperatureBy = function(degrees) {
+  this._temperature -= degrees
+  if (this.temperature < 10) this._temperature = 10
+}
+Thermostat.prototype.reset = function() {
+  this._temperature = 20
+}
+Thermostat.prototype.colorise = function(temperature) {
+  var rainbow = new Rainbow()
+  rainbow.setSpectrum('green','yellow','red')
+  rainbow.setNumberRange(10,35)
+  return rainbow.colourAt(temperature)
+}
+
